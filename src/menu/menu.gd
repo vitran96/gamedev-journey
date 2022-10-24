5e5555afc6f2bc9c6b3a8b4e815cd23a5f907e54
vitran96 <tranuytrieuvi@gmail.com>
2022-10-24 16:07:30 +0700

    feature: #3 - overlay menu implemented

---
+++ src/menu/menu.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+onready var overlay_node = $Overlay
+onready var message_node = $MessageContainer
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event.is_action_pressed("ui_pause"):
+#		get_tree().paused = !get_tree().paused
+#		overlay_node.visible = get_tree().paused
+#		message_node.visible = get_tree().paused
+
+
