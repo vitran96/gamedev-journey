9e9795325b7cb85126d5af3cc7e8d2e0ad47ef18
vitran96 <tranuytrieuvi@gmail.com>
2022-10-24 16:14:41 +0700

    refactor: reduce node query in main

--- src/main.gd
+++ src/main.gd
@@ -7,29 +7,36 @@ const BALL := preload("res://ball/ball.tscn")
 
 var rng = RandomNumberGenerator.new()
 
+onready var hud_node := $HUD
+
+var player_1_node: Node = null
+var player_2_node: Node = null
+
+var ball_node: Node = null
+
 func _ready() -> void:
-	$HUD.set_score_text(PlayerTypes.PLAYER_1, GlobalVars.player_1_score)
-	$HUD.set_score_text(PlayerTypes.PLAYER_2, GlobalVars.player_2_score)
+	hud_node.set_score_text(PlayerTypes.PLAYER_1, GlobalVars.player_1_score)
+	hud_node.set_score_text(PlayerTypes.PLAYER_2, GlobalVars.player_2_score)
 
 	var window_size := GlobalSettings.get_window_size()
 	var middle_y := window_size.y / 2
 
-	var player_1 := create_player(Vector2(GlobalVars.PLAYER_POSITION_OFFSET, middle_y), PlayerTypes.PLAYER_1)
-	player_1.name = "Player1"
-	add_child(player_1)
+	player_1_node= create_player(Vector2(GlobalVars.PLAYER_POSITION_OFFSET, middle_y), PlayerTypes.PLAYER_1)
+	player_1_node.name = "Player1"
+	add_child(player_1_node)
 
 	var death_zone_1 := create_death_zone(Vector2(0 - GlobalVars.DEATH_ZONE_OFFSET, middle_y), PlayerTypes.PLAYER_1)
 	add_child(death_zone_1)
 
-	var player_2 := create_player(Vector2(window_size.x - GlobalVars.PLAYER_POSITION_OFFSET, middle_y), PlayerTypes.PLAYER_2)
-	player_2.name = "Player2"
-	add_child(player_2)
+	player_2_node= create_player(Vector2(window_size.x - GlobalVars.PLAYER_POSITION_OFFSET, middle_y), PlayerTypes.PLAYER_2)
+	player_2_node.name = "Player2"
+	add_child(player_2_node)
 
 	var death_zone_2 := create_death_zone(Vector2(window_size.x + GlobalVars.DEATH_ZONE_OFFSET, middle_y), PlayerTypes.PLAYER_2)
 	add_child(death_zone_2)
 
-	var ball := create_ball(window_size / 2)
-	add_child(ball)
+	ball_node = create_ball(window_size / 2)
+	add_child(ball_node)
 
 func create_player(position: Vector2, player_type: int) -> Node:
 	var player := PLAYER.instance()
@@ -56,24 +63,23 @@ func create_ball(position: Vector2) -> Node:
 func reset_player_position() -> void:
 	var window_size := GlobalSettings.get_window_size()
 	var middle_y := window_size.y / 2
-	$Player1.position = Vector2(GlobalVars.PLAYER_POSITION_OFFSET, middle_y)
-	$Player2.position = Vector2(window_size.x - GlobalVars.PLAYER_POSITION_OFFSET, middle_y)
+	player_1_node.position = Vector2(GlobalVars.PLAYER_POSITION_OFFSET, middle_y)
+	player_2_node.position = Vector2(window_size.x - GlobalVars.PLAYER_POSITION_OFFSET, middle_y)
 
 func reset_ball() -> void:
-	var ball = $Ball
-	remove_child(ball)
-	ball.queue_free()
+	remove_child(ball_node)
+	ball_node.queue_free()
 	var window_size := GlobalSettings.get_window_size()
-	ball = create_ball(window_size / 2)
-	add_child(ball)
+	ball_node = create_ball(window_size / 2)
+	add_child(ball_node)
 
 func _on_hit_death_zone(player_type: int) -> void:
 	match player_type:
 		PlayerTypes.PLAYER_1:
 			GlobalVars.player_2_score += 1
-			$HUD.set_score_text(PlayerTypes.PLAYER_2, GlobalVars.player_2_score)
+			hud_node.set_score_text(PlayerTypes.PLAYER_2, GlobalVars.player_2_score)
 		PlayerTypes.PLAYER_2:
 			GlobalVars.player_1_score += 1
-			$HUD.set_score_text(PlayerTypes.PLAYER_1, GlobalVars.player_1_score)
+			hud_node.set_score_text(PlayerTypes.PLAYER_1, GlobalVars.player_1_score)
 	reset_player_position()
 	reset_ball()
