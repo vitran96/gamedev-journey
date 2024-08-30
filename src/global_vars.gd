02fe92d311c063fb8d0a4016e67e020fbd1e7e47
vitran96 <tranuytrieuvi@gmail.com>
2024-08-30 08:32:39 +0700

    remove everything

--- src/global_vars.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends Node2D
-
-export var PLAYER_POSITION_OFFSET := 30
-export var DEATH_ZONE_OFFSET := 10
-
-export var player_1_score := 0
-export var player_2_score := 0
--- src/player_types.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-const NONE = 0
-const PLAYER_1 = 1
-const PLAYER_2 = 2
