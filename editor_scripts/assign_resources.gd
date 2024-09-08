8850835008b50e2cab90636d359349f0dcee2df6
vitran96 <tranuytrieuvi@gmail.com>
2024-09-08 09:09:55 +0700

    finish basic resource

--- editor_scripts/assign_resources.gd
+++ editor_scripts/assign_resources.gd
@@ -2,8 +2,9 @@ tool
 extends EditorScript
 
 const player_scene := preload("res://player.tscn")
+const boundary_scene := preload("res://boundary.tscn")
 
-func _run():
+func _run() -> void:
 	if not Engine.editor_hint:
 		print("This script only run once to init resource")
 		return
@@ -14,30 +15,69 @@ func _run():
 		print("This script only work with Main game screen")
 		return
 
+	var game_height: int = ProjectSettings.get_setting("display/window/size/height")
+	var game_width: int = ProjectSettings.get_setting("display/window/size/width")
+
 	var children := scene.get_children()
+
+	var boundary1: Node
+	var boundary2: Node
+
+	var player1: Node
+	var player2: Node
 
 	# Delete player node
 	for child in children:
 		if child is Player:
-			scene.remove_child(child)
-			child.queue_free()
+#			scene.remove_child(child)
+#			child.queue_free()
+			if child.name == "Player1":
+				player1 = child
+			elif child.name == "Player2":
+				player2 = child
+		elif child is Boundary:
+			if child.name == "TopBoundary":
+				boundary1 = child
+			elif child.name == "BottomBoundary":
+				boundary2 = child
 
-	# Add new player node
-	var player1: Node2D = player_scene.instance()
-	player1.name = "Player1"
-	scene.add_child(player1)
-	player1.owner = scene
-	player1.position = Vector2(0, 0)
+	# Add boundary
+	if not boundary1:
+		boundary1 = _create_boundary(scene, 0, "TopBoundary")
+
+	if not boundary2:
+		boundary2 = _create_boundary(scene, game_height - 10, "BottomBoundary")
+
+	# Add player
+	var player_y = game_height / 2 - 130 / 2
+	if not player1:
+		player1 = _create_player(scene, "Player1")
+
+	player1.position = Vector2(10, player_y)
+
+	if not player2:
+		player2 = _create_player(scene, "Player2")
+
+	player2.position = Vector2(game_width - 10 - 15, player_y)
 
 
-	var player2 := player_scene.instance()
-	player2.name = "Player2"
-	scene.add_child(player2)
-	player2.owner = scene
-	player2.position = Vector2(100, 0)
 
-	print("Players added to GameScreen.")
+	print("DONE")
 
+func _create_boundary(root: Node, y: int, name: String) -> Node:
+	var boundary := boundary_scene.instance()
 
+	root.add_child(boundary)
+	boundary.name = name
+	boundary.position = Vector2(0, y)
+	boundary.owner = root
 
+	return boundary
 
+func _create_player(root: Node, name: String) -> Node:
+	var player: Node = player_scene.instance()
+	player.name = name
+	root.add_child(player)
+	player.owner = root
+
+	return player
--- editor_scripts/resize_boundary.gd
+++ editor_scripts/resize_boundary.gd
@@ -3,8 +3,7 @@ extends EditorScript
 
 const player_scene := preload("res://player.tscn")
 
-const WIDTH = 15
-const HEIGHT = 120
+const HEIGHT = 10
 
 const BOUNDARY_HEIGHT = 10
 
@@ -16,15 +15,15 @@ func _run():
 	var scene := get_scene()
 #	var scene2 = get_editor_interface().get_edited_scene_root()
 
-	if scene.name != "Player":
-		print("This script only work with Player screen")
+	if scene.name != "Boundary":
+		print("This script only work with Boundary screen")
 		return
 
 #	var height: int = ProjectSettings.get_setting("display/window/size/height")
-#	var width: int = ProjectSettings.get_setting("display/window/size/width")
+	var width: int = ProjectSettings.get_setting("display/window/size/width")
 
 	var color_rect = scene.get_node("ColorRect") as ColorRect
-	color_rect.rect_size.x = WIDTH
+	color_rect.rect_size.x = width
 	color_rect.rect_size.y = HEIGHT
 
 	print("DONE")
